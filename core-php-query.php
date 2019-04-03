62f784cb976cc73c603fc94754a3e8f5bf2d1e1c
vishnu6666 <40343138+vishnu6666@users.noreply.github.com>
2019-04-03 17:40:45 +0530

    Update core-php-query.php

--- core-php-query.php
+++ core-php-query.php
@@ -1,12 +1,11 @@
-public function get_wishlist_status($listing_id)
-    {
-        $this->db->select('*')
-            ->from('wishlist')
-            ->where('listing_id', $listing_id);
-        $query = $this->db->get();
-        if ($query->num_rows() > 0) {
-            return 'wish_bt liked';
-        } else {
-            return 'wish_bt';
-        }
-    }
+This is JOin query for more the two table  
+$query="SELECT listing.id as listingid,listing.listing_title,listing.category,listing.form_type,listing.keywords,listing.description,listing.photo,listing.city,listing.state,listing.map_lat,listing.map_long,review.listing_id,review.rating_review,AVG(review.rating_review) AS review_avg ,count(review.listing_id),category.category_name ,category.id AS review_count
+                        FROM wishlist                        
+                        JOIN listing  ON wishlist.listing_id = listing.id
+                        LEFT JOIN review ON wishlist.listing_id = review.listing_id
+                        JOIN category ON category.id = listing.category 
+                        WHERE wishlist.user_id=$user_id
+                        GROUP BY listing.id
+                        ORDER BY wishlist.wishlist_id  DESC  LIMIT 0, 100 
+                        ";
+  $query_result = $this->db->query($query);
