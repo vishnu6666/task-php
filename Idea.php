82ae1577d13a7760eac3005865c554377608ee23
vishnu6666 <40343138+vishnu6666@users.noreply.github.com>
2019-04-08 17:16:45 +0530

    Update Idea.php

--- Idea.php
+++ Idea.php
@@ -1,1 +1,12 @@
-sdfsda fsdf sdf sdf sdf sdf
+**************************************************************************************************************
+API REFERENCE 
+**************************************************************************************************************
+https://www.youtube.com/watch?v=DHUxnUX7Y2Y
+
+CODE -https://github.com/bradtraversy/slimapp
+
+RELATED COURSE - http://www.traversymedia.com/course/l...
+
+SlimPHP Site - https://www.slimframework.com/
+
+Get Composer - https://getcomposer.org/
